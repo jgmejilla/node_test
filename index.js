eaf693de916db1395ff4c174b6a155180151189a
jgmejilla <jgmejilla@up.edu.ph>
2024-12-05 04:51:10 +0800

    test

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const server = http.createServer((req, res) => {
     }
   })
 
-  res.writeHead(200, { 'Content-Type': 'text/html'});
-  res.end(file)
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.end(req.url + '\n' + file)
 });
 
 server.listen(8000);
